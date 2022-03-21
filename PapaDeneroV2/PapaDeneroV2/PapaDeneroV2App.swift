0a1a322b66c404d52bc74363fa02fe7c570ba598
Papa Denero <lozingyt@gmail.com>
2022-03-21 12:10:51 +0000

    ethers

---
+++ PapaDeneroV2/PapaDeneroV2/PapaDeneroV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PapaDeneroV2App.swift
+//  PapaDeneroV2
+//
+//  Created by LAURENCE BRADSHAW on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PapaDeneroV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
